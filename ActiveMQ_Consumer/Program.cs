7b3d0e813014d5a77f650d41407f23e8228dfec5
kazemi <kazemi.sm@refah-td.com>
2022-06-28 16:18:23 +0430

    Add project files.

---
+++ ActiveMQ_Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Apache.NMS;
+using Apache.NMS.Util;
+using System.Text;
+
+
+recieve();
+
+static void recieve()
+{
+    IConnectionFactory connectionFactory = new NMSConnectionFactory("tcp://localhost:61616");
+    IConnection connection = connectionFactory.CreateConnection();
+    connection.Start();
+    ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+
+    //============================ Various Destinations
+
+    //IDestination destination = SessionUtil.GetDestination(session, "topic://MyTopic");
+    //IDestination destination = SessionUtil.GetDestination(session, "queue://MyQueue");
+    //IDestination destination = SessionUtil.GetTopic(session, "MyTopic");
+    IDestination destination = SessionUtil.GetQueue(session, "MyQueue");
+
+    //============================ Various Destinations
+
+    IMessageConsumer consumer = session.CreateConsumer(destination);
+
+    consumer.Listener += Consumer_Listener;
+    Console.Read();
+
+}
+
+static void Consumer_Listener(IMessage message)
+{
+    try
+    {
+        IBytesMessage? bytesMessage = message as IBytesMessage;
+        Console.WriteLine("Message received: {0}", Encoding.ASCII.GetString(bytesMessage.Content));
+    }
+    catch (Exception ex)
+    { }
+}
---
+++ ActiveMQ_Sample/Program.cs
@@ -0,0 +1,40 @@
+﻿using Apache.NMS;
+using Apache.NMS.Util;
+using System.Text;
+
+
+send();
+
+static void send()
+{
+
+    while (true)
+    {
+        Console.Write("Enter Your Message: ");
+        string? Message = Console.ReadLine();
+
+        if (Message is null || Message?.ToLower() == "exit")
+            break;
+
+        IConnectionFactory connectionFactory = new NMSConnectionFactory("tcp://localhost:61616");
+        IConnection connection = connectionFactory.CreateConnection();
+        connection.Start();
+        ISession session = connection.CreateSession();
+
+        //============================ Various Destinations
+
+        //IDestination destination = SessionUtil.GetDestination(session, "topic://MyTopic");
+        //IDestination destination = SessionUtil.GetDestination(session, "queue://MyQueue");
+        //IDestination destination = SessionUtil.GetTopic(session, "MyTopic");
+        IDestination destination = SessionUtil.GetQueue(session, "MyQueue");
+
+        //============================ Various Destinations
+
+        IMessageProducer producer = session.CreateProducer(destination);
+        IBytesMessage bytesMessage = session.CreateBytesMessage(Encoding.ASCII.GetBytes(Message));
+
+        producer.Send(bytesMessage);
+        session.Close();
+        connection.Stop();
+    }
+}
